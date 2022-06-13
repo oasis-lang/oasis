19d87796b52204f566deb7f753956a54c8f54f39
Mason Meirs <maseymoose@gmail.com>
2022-06-12 20:31:27 -0400

    a lot

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_redactor_theme
+html_theme = 'sphinx_redactor_theme'
+html_theme_path = [sphinx_redactor_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
