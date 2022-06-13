fe7ed0590da549ba0b288179522bd8d98dafe882
Mason Meirs <maseymoose@gmail.com>
2022-06-12 20:51:25 -0400

    a lot

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 
 html_theme = 'press'
-html_theme_path = [sphinx_redactor_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
