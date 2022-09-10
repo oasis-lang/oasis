877b345523b797d1d7e548331399551ef8314300
snwy <maseymoose@gmail.com>
2022-09-10 02:40:06 -0400

    gpio module

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module oasisgo
 
 go 1.18
 
-require github.com/stianeikeland/go-rpio v4.2.0+incompatible
+require gobot.io/x/gobot v1.16.0
+
+require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+)
--- stdlib/gpio.go
+++ stdlib/gpio.go
@@ -1,128 +1,198 @@
 package stdlib
 
 import (
-	"github.com/stianeikeland/go-rpio"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
 	"oasisgo/core"
 )
 
 type GPIO struct{}
 
 func (GPIO) Create(vm *core.VM) (string, any) {
-	return "gpio", &core.Prototype{
+	r := raspi.NewAdaptor()
+	return "GPIO", &core.Prototype{
 		Inherited: &core.BasePrototype,
 		Body: map[string]any{
 			"init": &core.NativeFunction{
 				Fn: func(vm *core.VM, args []any) any {
-					err := rpio.Open()
-					if err != nil {
-						return CreateResult(vm, nil, true, err.Error())
-					}
-					return CreateResult(vm, nil, false, "")
-				},
-				Args: 0,
-			},
-			"done": &core.NativeFunction{
-				Fn: func(vm *core.VM, args []any) any {
-					err := rpio.Close()
-					if err != nil {
-						return CreateResult(vm, nil, true, err.Error())
-					}
-					return CreateResult(vm, nil, false, "")
-				},
-				Args: 0,
-			},
-			"pin": &core.NativeFunction{
-				Fn: func(vm *core.VM, args []any) any {
-					pin := rpio.Pin(args[0].(int))
-					var freq int
 					return &core.Prototype{
 						Inherited: &core.BasePrototype,
 						Body: map[string]any{
-							"__pin": pin,
-							"input": &core.NativeFunction{
+							"__raspi": r,
+							"led": &core.NativeFunction{
 								Fn: func(vm *core.VM, args []any) any {
-									pin.Input()
-									return nil
-								},
-								Args: 0,
-							},
-							"output": &core.NativeFunction{
-								Fn: func(vm *core.VM, args []any) any {
-									pin.Output()
-									return nil
-								},
-								Args: 0,
-							},
-							"high": &core.NativeFunction{
-								Fn: func(vm *core.VM, args []any) any {
-									pin.High()
-									return nil
-								},
-								Args: 0,
-							},
-							"low": &core.NativeFunction{
-								Fn: func(vm *core.VM, args []any) any {
-									pin.Low()
-									return nil
-								},
-								Args: 0,
-							},
-							"read": &core.NativeFunction{
-								Fn: func(vm *core.VM, args []any) any {
-									val := pin.Read()
-									if val == rpio.High {
-										return true
+									led := gpio.NewLedDriver(r, args[0].(string))
+									return &core.Prototype{
+										Inherited: &core.BasePrototype,
+										Body: map[string]any{
+											"__led": led,
+											"__add_to_bot": func() gobot.Device {
+												return led
+											},
+											"on": &core.NativeFunction{
+												Fn: func(vm *core.VM, args []any) any {
+													err := led.On()
+													if err != nil {
+														return CreateResult(vm, nil, true, err.Error())
+													}
+													return CreateResult(vm, nil, false, "")
+												},
+												Args: 0,
+											},
+											"off": &core.NativeFunction{
+												Fn: func(vm *core.VM, args []any) any {
+													err := led.Off()
+													if err != nil {
+														return CreateResult(vm, nil, true, err.Error())
+													}
+													return CreateResult(vm, nil, false, "")
+												},
+												Args: 0,
+											},
+										},
 									}
-									return false
 								},
-								Args: 0,
+								Args: 1,
 							},
-							"write": &core.NativeFunction{
+							"button": &core.NativeFunction{
 								Fn: func(vm *core.VM, args []any) any {
-									if args[0].(bool) {
-										pin.Write(rpio.High)
-									} else {
-										pin.Write(rpio.Low)
+									button := gpio.NewButtonDriver(r, args[0].(string))
+									return &core.Prototype{
+										Inherited: &core.BasePrototype,
+										Body: map[string]any{
+											"__button": button,
+											"__add_to_bot": func() gobot.Device {
+												return button
+											},
+											"on": &core.NativeFunction{
+												Fn: func(vm *core.VM, args []any) any {
+													err := button.On(args[0].(string), func(data interface{}) {
+														args[1].(core.OasisCallable).Call(vm, []any{data})
+													})
+													if err != nil {
+														return CreateResult(vm, nil, true, err.Error())
+													}
+													return CreateResult(vm, nil, false, "")
+												},
+												Args: 2,
+											},
+										},
 									}
-									return nil
 								},
 								Args: 1,
 							},
-							"pwm": &core.NativeFunction{
+							"servo": &core.NativeFunction{
 								Fn: func(vm *core.VM, args []any) any {
-									pin.Pwm()
-									return nil
+									servo := gpio.NewServoDriver(r, args[0].(string))
+									return &core.Prototype{
+										Inherited: &core.BasePrototype,
+										Body: map[string]any{
+											"__servo": servo,
+											"__add_to_bot": func() gobot.Device {
+												return servo
+											},
+											"move": &core.NativeFunction{
+												Fn: func(vm *core.VM, args []any) any {
+													err := servo.Move(uint8(args[0].(int)))
+													if err != nil {
+														return CreateResult(vm, nil, true, err.Error())
+													}
+													return CreateResult(vm, nil, false, "")
+												},
+												Args: 1,
+											},
+											"min": &core.NativeFunction{
+												Fn: func(vm *core.VM, args []any) any {
+													err := servo.Min()
+													if err != nil {
+														return CreateResult(vm, nil, true, err.Error())
+													}
+													return CreateResult(vm, nil, false, "")
+												},
+												Args: 0,
+											},
+											"max": &core.NativeFunction{
+												Fn: func(vm *core.VM, args []any) any {
+													err := servo.Max()
+													if err != nil {
+														return CreateResult(vm, nil, true, err.Error())
+													}
+													return CreateResult(vm, nil, false, "")
+												},
+												Args: 0,
+											},
+											"center": &core.NativeFunction{
+												Fn: func(vm *core.VM, args []any) any {
+													err := servo.Center()
+													if err != nil {
+														return CreateResult(vm, nil, true, err.Error())
+													}
+													return CreateResult(vm, nil, false, "")
+												},
+												Args: 0,
+											},
+										},
+									}
+								},
+							},
+						},
+					}
+				},
+			},
+			"create": &core.NativeFunction{
+				Fn: func(vm *core.VM, args []any) any {
+					devicesRaw := args[1].(core.OasisList)
+					devices := make([]gobot.Device, len(*devicesRaw))
+					for i, deviceRaw := range *devicesRaw {
+						device, ok := deviceRaw.(*core.Prototype)
+						if !ok {
+							return CreateResult(vm, nil, true, "Expected device to be a prototype")
+						}
+						addToBot, err := device.Get("__add_to_bot")
+						if err != nil {
+							return CreateResult(vm, nil, true, "Not a device")
+						}
+						addToBotFn, ok := addToBot.(func() gobot.Device)
+						if !ok {
+							return CreateResult(vm, nil, true, "Not a device")
+						}
+						devices[i] = addToBotFn()
+					}
+					bot := gobot.NewRobot(args[0].(string),
+						[]gobot.Connection{r},
+						devices,
+						func() {
+							select {}
+						},
+					)
+					return &core.Prototype{
+						Inherited: &core.BasePrototype,
+						Body: map[string]any{
+							"__bot": bot,
+							"start": &core.NativeFunction{
+								Fn: func(vm *core.VM, args []any) any {
+									err := bot.Start()
+									if err != nil {
+										return CreateResult(vm, nil, true, err.Error())
+									}
+									return CreateResult(vm, nil, false, "")
 								},
 								Args: 0,
 							},
-							"freq": &core.NativeFunction{
+							"stop": &core.NativeFunction{
 								Fn: func(vm *core.VM, args []any) any {
-									pin.Freq(args[0].(int))
-									freq = args[0].(int)
-									return nil
-								},
-								Args: 1,
-							},
-							"dutyCycle": &core.NativeFunction{
-								Fn: func(vm *core.VM, args []any) any {
-									// calculate duty cycle from percentage
-									dutyCycle := uint32(args[0].(int)) * uint32(freq) / 100
-									pin.DutyCycle(dutyCycle, uint32(freq))
-									return nil
-								},
-								Args: 1,
-							},
-							"toggle": &core.NativeFunction{
-								Fn: func(vm *core.VM, args []any) any {
-									pin.Toggle()
-									return nil
+									err := bot.Stop()
+									if err != nil {
+										return CreateResult(vm, nil, true, err.Error())
+									}
+									return CreateResult(vm, nil, false, "")
 								},
 								Args: 0,
 							},
 						},
 					}
 				},
-				Args: 1,
 			},
 		},
 	}
